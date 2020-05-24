e9a5f8f2cc1568d485769c99c77e1af34d855010
cijin <cijininurhood@gmail.com>
2020-05-24 19:54:43 +0700

    Contacts component working. Next step create util to connect db.

--- src/components/Contacts/Contact/Contact.tsx
+++ src/components/Contacts/Contact/Contact.tsx
@@ -3,16 +3,20 @@ import './contact.css';
 
 type ContactProps = {
     contact: {name: string, status: string},
-    key: string,
-};
-
-type ContactStates = {
-
+    index: number,
+    toggleActive: Function,
+    isActive: boolean,
 };
 
 export default function Contact (props: ContactProps) {    
+    function handleClick() {
+        props.toggleActive(props.index);
+    }
+
     return (
-        <div className="contact">
+        <div className="contact" 
+            id={props.isActive ? "active" : "inactive"}
+            onClick={handleClick} >
             <p>{props.contact.name}</p>
             <p className="status">{props.contact.status}</p>
         </div>
--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -3,31 +3,81 @@ import Contact from './Contact/Contact';
 
 import './contacts.css';
 
-export default function Contacts () {   
-    let contacts: {name: string, status: string}[] = [{
+type Contact = {
+    name: string,
+    status: string, 
+    active: boolean,
+};
+
+export default function Contacts () { 
+    let prevContact: number = -1
+    let currentContact: number = -1
+
+    const [contacts, setContacts] = React.useState([]);
+
+    let contactsArray: Contact[] = [{
         name: "Viivi",
-        status: "Feeling lucky"
+        status: "At the stable",
+        active: false,
     }, 
     {
         name: "Linda",
-        status: "Cooking"
+        status: "Cooking",
+        active: false,
     },
     {
         name: "Fabid",
-        status: "Racing..."
+        status: "Racing...",
+        active: false,
     },
     {
         name: "Jon",
-        status: "Diving right now"
+        status: "Diving right now",
+        active: false,
+    }];
+    
+    function contactSelected(key: number) {             
+        if (currentContact === -1 && prevContact === -1) {
+            currentContact = key;
+            contactsArray[key].active = true;
+            setContacts(contactsArray);
+        }
+
+        else if (currentContact === key) {
+            return;
+        }
+
+        else {
+            prevContact = currentContact;
+            contacts[currentContact].active = false;
+            currentContact = key;
+            contacts[key].active = true;
+        }
     }
-]
+
+    function appendContacts() {
+        contactsArray.map(contact => {
+            setContacts(contacts => [
+                ...contacts,
+                contact,
+            ])
+        });
+    }
+
+    React.useEffect(() => {
+       appendContacts(); 
+    }, [])
         
     return (
         <div className="contacts">
             <h3>Contacts</h3>
             {
                 contacts.map((contact, index) => {
-                   return <Contact key={String(index)} contact={contact} />
+                   return <Contact toggleActive={contactSelected}
+                        isActive={ contact.active } 
+                        key={index} 
+                        index={index}
+                        contact={contact} />
                 })
             }            
         </div>
