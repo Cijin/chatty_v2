73ab50d926dc3de273b1fedaffd537d48b1b7306
cijin <cijininurhood@gmail.com>
2020-05-28 10:22:37 +0700

    Got localhost to load the html file.

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require('express');
 const app = express();
 const apiRouter = require('./api/api');
 const path = require('path');
-const serveStatic = require('serve-static');
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const errorHandler = require('errorhandler');
 
-const baseDir = path.resolve(__dirname, "./");
+const baseDir = path.resolve(__dirname, "");
+const distDir = path.join(baseDir, "/dist");
+//this is temporary, retrieving styles like this
+const styleDir = path.join(baseDir, "/public/css");
+
 const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
@@ -20,7 +23,20 @@ app.use(morgan('dev'));
 app.use('/api', apiRouter);
 app.use(errorHandler());
 
-app.get("*", (req, res) => {
+//there's got to be a better way to do this?
+app.get("/public/css/style.css", (req, res) => {
+    res.sendFile(path.join(styleDir, "/style.css"))
+})
+
+app.get("/public/css/reset.css", (req, res) => {
+    res.sendFile(path.join(styleDir, "/reset.css"))
+})
+
+app.get("/dist/bundle.js", (req, res) => {
+    res.sendFile(path.join(distDir, "/bundle.js"))
+})
+
+app.get("*", (req, res) => {    
     res.sendFile(path.join(baseDir, "/index.html"));
 });
 
